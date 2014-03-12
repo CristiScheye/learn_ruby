d158feba067b093fde77639caac53b497b2fc59f
Cristi Scheye <cristi.scheye@gmail.com>
2014-03-12 15:26:18 -0700

    repeating exercise 03

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,20 +1,14 @@
-def echo(str)
-  str
+def echo(phrase)
+  phrase
 end
 
-def shout(str)
-  str.upcase
+def shout(phrase)
+  phrase.upcase
 end
 
-def repeat(str, rep = 1)
-
-  if rep == 1
-    return str + " #{str}"
-  else
-    full = str
-    (rep - 1).times {full += " #{str}"}
-    full
-  end
+def repeat(phrase, rep=2)
+  phrase_reps = Array.new(rep, phrase)
+  phrase_reps.join(' ')
 end
 
 def start_of_word(word, num_letters)
@@ -22,29 +16,22 @@ def start_of_word(word, num_letters)
 end
 
 def first_word(phrase)
-  words = phrase.split
-  words[0]
-end
-
-def capitalize(word)
-  word[0].upcase + word[1..-1]
+  words = phrase.split(' ')
+  words.first
 end
 
 def titleize(phrase)
-  words_not_to_capitalize = ['and', 'or', 'the', 'over', 'in', 'of']
-  words = phrase.split
+  words = phrase.split(' ')
 
-  first_word_in_phrase = true
-  titleized_words = words.map do |word|
-    if first_word_in_phrase
-      first_word_in_phrase = false
-      capitalize(word)
-    elsif words_not_to_capitalize.include?(word)
+  proper_words = words.map.with_index do |word, index|
+    if index == 0
+      word[0].upcase + word[1..-1]
+    elsif ['the', 'and', 'an', 'by', 'over'].include? word
       word
     else
-      capitalize(word)
+      word[0].upcase + word[1..-1]
     end
   end
 
-  titleized_words.join(' ')
+  proper_words.join(' ')
 end
