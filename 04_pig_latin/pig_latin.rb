d56cca209d1208a6e4b8f012cf10994e434bd088
Cristi Scheye <cristi.scheye@gmail.com>
2014-03-12 15:52:38 -0700

    refactor with some more clever regexp! completing exercise 04

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,38 +1,24 @@
-require 'pry'
-
 def translate(phrase)
-  words = phrase.split
-  words.map! {|word| translate_word(word)}
-  words.join(' ')
-end
+  words = phrase.split(' ')
 
+  pig_words = words.map do |word|
 
-def translate_word(word)
-  num_const = count_consonants(word)
-  if num_const == 0
-    word + 'ay'
-  else
-    word[num_const..-1] + word[0..(num_const-1)] + 'ay'
-  end
-end
+    first_vowel = /[aeiouAEIOU]/ =~ word
+    first_qu = /qu/ =~ word
 
+    if !!first_qu && first_qu < first_vowel
+      first_vowel = first_qu + 2 
+    end
 
-def count_consonants(word)
-  if word[0..2] == 'squ'
-    return 3
-  elsif word[0..1] == 'qu'
-    return 2
-  else
-    count = 0
-  end
-
-  word.each_char do |char|
-    if /[aeiou]/ === char.downcase
-      break
+    if first_vowel == 0
+      word + 'ay'
     else
-      count += 1
+      word[first_vowel..-1] + word[0...first_vowel] + 'ay'
     end
   end
 
-  count
+  pig_words.join(' ')
 end
+
+
+
